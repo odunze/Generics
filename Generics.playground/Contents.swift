fc008d8b67c0d1664a3dc8edee88c4326de8cdb8
Lotanna <sugabelly@gmail.com>
2018-11-27 23:27:27 -0500

    Initial commit

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+func swapNumbers(a: Int, b: Int) {
+    
+    var first = a
+    var second = b
+    
+    first = first + second
+    
+    second = first - second
+    
+    first = first - second
+    
+    print(first)
+    print(second)
+
+}
+
+swapNumbers(a: 7, b: 9)
+

d03d68d17972892f6368698a3acfc4cd4fe1ae14
Lotanna <sugabelly@gmail.com>
2019-04-02 19:30:38 -0400

    xcode updates

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -28,3 +28,12 @@ extension CountedSet {
     return 0
     }
 }
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
